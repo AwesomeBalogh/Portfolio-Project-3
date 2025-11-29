cd18439329bda5f8f67bb6c788c9b10c00f64c6c
Bennett Balogh <bennettsbalogh@gmail.com>
2025-11-29 13:29:34 -0700

    Added Resorts in region and fixed issue with two word contries not being accepted with .title() instead of .capitalize()

--- main.py
+++ main.py
@@ -4,19 +4,17 @@ import matplotlib as plt
 
 import user_csv
 
-#test 
-
 def get_region_input():
     resort_data = user_csv.read_csv("resorts", 1)      #gets csv of ski resort data
     while True:
-        input_region = input("input your country here: ").capitalize().strip()     #Ask for input regions
+        input_region = input("input your country here: ").title().strip()     #Ask for input regions
+
         valid_regions = []                                                  
         for i in range(len(resort_data)):                           #iterates through the length of the ski resorts and indexes the locations to a valid region list
             if(resort_data[i][4] not in valid_regions):             #removes duplicate countries from valid regions list
                 valid_regions.append(resort_data[i][4])
 
         valid_regions.sort()                                        #alphabetize list
-
         if(input_region == '0'):
             for i in range(len(valid_regions)):                     #print nicely formatted list
                 print(f'{i} - {valid_regions[i]}')
@@ -27,13 +25,29 @@ def get_region_input():
 
         else:
             print("valid")
+            return input_region
             break
 
 
     return input_region
 
+def resorts_in_region(region):
+    resort_data = user_csv.read_csv("resorts", 1)     #gets csv of ski resort data
+    resorts_list = []                                   #empty list to store resorts in the specified region
+    for row in resort_data:
+        mountain_region = row[4]                        #indexing the region column
+        mountain_name = row[1]                          #indexing the resort name column
+        if mountain_region == region:                   #checking if the resort's region matches the input region
+            resorts_list.append(mountain_name)          #adding the resort name to the list if it matches
 
-get_region_input()
+    print(f"Resorts in {region}:")
+    resorts_list.sort()                                        #alphabetize list
+    for i in range(len(resorts_list)):                     #print nicely formatted list
+        print(f'{i} - {resorts_list[i]}')
+    return resorts_list
+
+input_region = get_region_input()
+resorts_in_region(input_region)
 
 
 '''
