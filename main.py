c646c8627182bee4a1c8f376d6f16a754ff02526
Bennett Balogh <bennettsbalogh@gmail.com>
2025-11-29 21:29:20 -0700

    Took valid regions out of while true to make it more efficeint. Added rows list function for easier coding later

--- main.py
+++ main.py
@@ -6,44 +6,49 @@ import user_csv
 
 def get_region_input():
     resort_data = user_csv.read_csv("resorts", 1)      #gets csv of ski resort data
-    while True:
-        input_region = input("input your country here: ").title().strip()     #Ask for input regions
-
-        valid_regions = []                                                  
-        for i in range(len(resort_data)):                           #iterates through the length of the ski resorts and indexes the locations to a valid region list
-            if(resort_data[i][4] not in valid_regions):             #removes duplicate countries from valid regions list
-                valid_regions.append(resort_data[i][4])
-
+    valid_regions = []                                                  
+    for i in range(len(resort_data)):                           #iterates through the length of the ski resorts and indexes the locations to a valid region list
+        if(resort_data[i][4] not in valid_regions):             #removes duplicate countries from valid regions list
+            valid_regions.append(resort_data[i][4])
         valid_regions.sort()                                        #alphabetize list
+    
+    while True:
+        input_region = input("Input your country here: ").title().strip()     #Ask for input regions
+
         if(input_region == '0'):
             for i in range(len(valid_regions)):                     #print nicely formatted list
                 print(f'{i} - {valid_regions[i]}')
             
         elif(not np.isin(input_region, valid_regions)):
-            print("Invalid region. Please enter a valid state or province from the following list:")
+            print("Invalid region. Please enter a valid country:")
             print("To print a list of valid regions enter 0")
 
         else:
             print("valid")
             return input_region
-            break
 
+def region_rows_list(region):
+    data = user_csv.read_csv("resorts", 1)             #gets csv of ski resort data             
+    rows = []
+    for i in range(len(data)):
+        if data[i][4] == region:
+            rows.append(data[i])
+    print(rows)    
+    return rows
 
-    return input_region
-
-def resorts_in_region(region):
+def resorts_in_region(country):
     resort_data = user_csv.read_csv("resorts", 1)     #gets csv of ski resort data
     resorts_list = []                                   #empty list to store resorts in the specified region
     for row in resort_data:
         mountain_region = row[4]                        #indexing the region column
         mountain_name = row[1]                          #indexing the resort name column
-        if mountain_region == region:                   #checking if the resort's region matches the input region
+        if mountain_region == country:                   #checking if the resort's region matches the input region
             resorts_list.append(mountain_name)          #adding the resort name to the list if it matches
 
-    print(f"Resorts in {region}:")
+    print(f"Resorts in {country}:")
     resorts_list.sort()                                        #alphabetize list
     for i in range(len(resorts_list)):                     #print nicely formatted list
-        print(f'{i} - {resorts_list[i]}')
+        print(f'{i + 1} - {resorts_list[i]}')
     return resorts_list
 
 def average_price():
@@ -58,9 +63,10 @@ def average_price():
 
 
 #Main Program
-average_price()
+
 input_region = get_region_input()
 resorts_in_region(input_region)
+
 
 
 
