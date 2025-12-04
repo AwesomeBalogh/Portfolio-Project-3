4fca0666e0512eac7b5bf855dbb12bb942a8b0b5
Jordan Kisiler <jordan.kisiler126@gmail.com>
2025-12-04 16:03:33 -0700

    Fixed User CSV to use no external libraries

--- user_csv.py
+++ user_csv.py
@@ -3,14 +3,29 @@ import numpy as np
 def read_csv(file_name, header):
     '''
     Take a CSV file and convert it into a 2D numpy array
-    Inputs: file name, header status
+    Inputs: file name, header status(0 to include header, 1 to exclude header)
     Outputs: numpy array of data
     '''
-    # fix for gen from tex
-    fname = f'{file_name}.csv'
-    array = np.genfromtxt((open(fname)), delimiter = ",", dtype = None, skip_header = header,)       #Make CSV numpy arrays (0 to include header, 1 to skip header)      
-    #
-    return array
+
+    header = int(header)            #ensure header is an int value
+
+    if (header == 0 or header == 1):
+        fname = f'{file_name}.csv'
+
+        raw_csv = open(fname, 'r', newline = '').read().strip().split('\r\n')       #read the csv, then remove extra whitespace, and split it by the new lines
+    
+        refined_csv = []
+
+        for i in range(len(raw_csv)- header):       #for every string from splitting by newline, split it further by the commas and append it to a new CSV
+            row = (raw_csv[i + header].split(','))
+            refined_csv.append(row)                 #append each resorts row to a list
+        
+        arr = np.array(refined_csv)             #convert list to numpy array
+
+        return arr
+        
+    else:
+        print('Error, please either input 0 to include header or 1 to remove headers')
 
 
 def write_csv(file_name, numpy_array, overwrite):       #for overwrite, input 'x' to make a new file, input 'a' to append 
@@ -30,7 +45,6 @@ def write_csv(file_name, numpy_array, overwrite):       #for overwrite, input 'x
         
         row = ",".join(row)         #join the elements of the list with a comma
         
-        
         file.write(row)             #write to the new file
         
         file.write('\n')            #adds newline character at the end of the row
@@ -40,8 +54,10 @@ def write_csv(file_name, numpy_array, overwrite):       #for overwrite, input 'x
 
 #=================================================================================================================================================
 #for testing only
+
 #uncomment to test read.csv
-#print(read_csv('resorts',1))
+#print(read_csv('resort runs', 0))
+
 
 #uncomment to test write.csv()
 #list = [['bennet','Jordan','Elle'],[102,203,21],[123,34,21]]
